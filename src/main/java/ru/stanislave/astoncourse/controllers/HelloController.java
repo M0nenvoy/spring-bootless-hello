f6d7ec12ad77ce08a8a2fcca8a1ad05267254c80
M0nenvoy <goldseeker68191@gmail.com>
2024-01-12 14:54:03 +0400

    "controllers" directory added

--- src/main/java/ru/stanislave/astoncourse/controllers/HelloController.java
+++ src/main/java/ru/stanislave/astoncourse/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package ru.stanislave.astoncourse;
+package ru.stanislave.astoncourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
